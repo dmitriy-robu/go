d1ae68396328279355ae61d3e14b6f5b83f63554
dmitriy_robu <dmitriyrobu@gmail.com>
2023-04-12 20:11:29 +0300

    fix

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"flag"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -12,10 +9,8 @@ import (
 	"go-rust-drop/internal/api/database/migrations"
 	"go-rust-drop/internal/api/database/mongodb"
 	"go-rust-drop/internal/api/routes"
-	"io"
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -25,25 +20,6 @@ func main() {
 		log.Fatalf("Error loading .env file: %v", err)
 		return
 	}
-
-	genKeyFlag := flag.Bool("genkey", false, "Generate a new session secret key and save it to .env")
-	flag.Parse()
-
-	if *genKeyFlag {
-		key, err := generateRandomKey(32)
-		if err != nil {
-			log.Fatalf("Failed to generate random key: %v", err)
-			return
-		}
-
-		if err = appendKeyToFile(key, ".env"); err != nil {
-			log.Fatalf("Failed to append key to .env file: %v", err)
-			return
-		}
-
-		log.Printf("New session secret key generated and saved to .env: %s", key)
-	}
-
 	r := gin.Default()
 
 	goth.UseProviders(
@@ -65,44 +41,4 @@ func main() {
 		log.Fatalf("Failed to run server: %v", err)
 		return
 	}
-}
-
-func generateRandomKey(length int) (string, error) {
-	key := make([]byte, length)
-
-	_, err := io.ReadFull(rand.Reader, key)
-	if err != nil {
-		return "", err
-	}
-
-	keyString := base64.StdEncoding.EncodeToString(key)
-
-	return keyString, nil
-}
-
-func appendKeyToFile(key string, envFile string) error {
-	content, err := os.ReadFile(envFile)
-	if err != nil {
-		return err
-	}
-
-	lines := strings.Split(string(content), "\n")
-	found := false
-
-	for i, line := range lines {
-		if strings.HasPrefix(line, "SESSION_SECRET=") {
-			lines[i] = "SESSION_SECRET=" + key
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		lines = append(lines, "SESSION_SECRET="+key)
-	}
-
-	newContent := strings.Join(lines, "\n")
-	err = os.WriteFile(envFile, []byte(newContent), 0644)
-
-	return err
 }
